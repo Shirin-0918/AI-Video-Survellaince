be803b759c649355596be09f236faea2183c9d0e
Shirin-0918 <69805254+Shirin-0918@users.noreply.github.com>
2020-09-21 19:59:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video=""
 status=""
+objects=[];
 function preload(){
    video=createVideo('video.mp4');
 }
@@ -10,6 +11,22 @@ function setup(){
 }
 function draw(){
     image(video,0,0,480,380);
+    if(status != ""){
+        objectDetector.detect(video, gotResults);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML="Status: Object Detected";
+            document.getElementById("objectsNum").innerHTML="No. of Objects:"+ objects.length;
+            r=random(255);
+            g=random(255);
+            b=random(255);
+            fill(r,g,b);
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x + 15, objects[i].y+15);
+            nofill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width, objects[i].height);
+        }
+    }
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd', modelLoaded);
@@ -22,4 +39,12 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResults(error, results){
+       if(error){
+           console.error(error);
+       }
+       else{
+           console.log(results);
+       }
 }
