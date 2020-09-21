70589505784886f26e8d2a32982556bb23ab25cc
Shirin-0918 <69805254+Shirin-0918@users.noreply.github.com>
2020-09-21 20:28:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function draw(){
             fill(r,g,b);
             percent=floor(objects[i].confidence*100);
             text(objects[i].label+" "+percent+"%",objects[i].x + 15, objects[i].y+15);
-            nofill();
+            noFill();
             stroke(r,g,b);
             rect(objects[i].x,objects[i].y,objects[i].width, objects[i].height);
         }
